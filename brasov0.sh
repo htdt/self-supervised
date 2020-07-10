763b39b48cfece30c8cc1903ed203025fb87122a
Alexander <i@numart.ru>
2020-07-10 12:38:51 +0300

    minor

--- brasov0.sh
+++
@@ -1,5 +0,0 @@
-python -m train --w_mse --epoch 100 --add_bn --add_bn_last
-python -m train --w_mse --epoch 100 --add_bn --lr 7e-4
-python -m train --w_mse --epoch 100 --add_bn --lr 5e-4
-python -m train --w_mse --epoch 100 --add_bn --lr 3e-4
-
--- brasov1.sh
+++
@@ -1,5 +0,0 @@
-python -m train --w_mse --epoch 100 --add_bn --T0 1 --Tmult 2
-python -m train --w_mse --epoch 100 --add_bn --T0 1
-python -m train --w_mse --epoch 100 --add_bn --T0 5
-python -m train --w_mse --epoch 100 --add_bn --T0 20
-
